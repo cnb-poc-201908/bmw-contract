4a7b3fafcafafa87e2a7a0c6aff7668fb895c6e3
KennyLi <kennylikai@163.com>
2019-08-07 21:35:53 +0800

    Add common classes

--- src/main/java/com/bmw/common/BMWPocConstants.java
+++ src/main/java/com/bmw/common/BMWPocConstants.java
@@ -7,13 +7,13 @@ public class BMWPocConstants {
 	}
 
 	public static final String ACCEPT_LANG_ZH_CH = "zh-CN";
-	public static final String API_V1 = "/v1";
-	public static final String API_V2 = "/v2";
-	public static final String KEY_NAME_VALUE = "value";
-	public static final Integer REST_ERROR_CODE = -1;
-	public static final Integer REPAIRORDER_PAGE_SIZE = 200;
-	public static final String DEFAULT_JULIAN_DATE = "2458000";
-	public static final Integer STOCK_PRICE_PER_DAY = 80;
-	public static final String KEY_NAME_STOCK_LIST = "stocklist";
-	public static final String KEY_NAME_STOCK360_LIST = "stock360list";
+//	public static final String API_V1 = "/v1";
+//	public static final String API_V2 = "/v2";
+//	public static final String KEY_NAME_VALUE = "value";
+//	public static final Integer REST_ERROR_CODE = -1;
+//	public static final Integer REPAIRORDER_PAGE_SIZE = 200;
+//	public static final String DEFAULT_JULIAN_DATE = "2458000";
+//	public static final Integer STOCK_PRICE_PER_DAY = 80;
+//	public static final String KEY_NAME_STOCK_LIST = "stocklist";
+//	public static final String KEY_NAME_STOCK360_LIST = "stock360list";
 }
--- src/main/java/com/bmw/controller/ContractController.java
+++ src/main/java/com/bmw/controller/ContractController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/kenny")
-public class DemoController {
+public class ContractController {
 
 	@GetMapping(value = "lee", produces = "application/json")
 	public String helloWorld() {
