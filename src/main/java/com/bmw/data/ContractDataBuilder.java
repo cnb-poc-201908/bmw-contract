97afc12bde46f5f2a1e44aabff071c26be438234
LIANG XIONG <xionglwh@cn.ibm.com>
2019-08-10 00:33:45 +0800

    Merge changes.

--- src/main/java/com/bmw/data/ContractDataBuilder.java
+++ src/main/java/com/bmw/data/ContractDataBuilder.java
@@ -13,13 +13,19 @@ import com.bmw.utils.DateUtil;
 import com.github.javafaker.Faker;
 
 public class ContractDataBuilder {
+	private static String[] dealerIds = new String[] { "DL-10006661", "D-10006662", "D-10006663", "D-10006664" };
+	private static String[] groupIds = new String[] { "GP-10001", "GP-10002", "GP-10003" };
+	private static String[] regionIds = new String[] { "RG-10001", "RG-10002" };
+	private static String[] dealerNames = new String[] { "北京京宝行汽车服务有限公司", "北京博瑞宝汽车销售服务有限公司", "深圳博瑞宝汽车销售服务有限公司",
+			"广州博瑞宝汽车销售服务有限公司" };
+	private static String[] groupNames = new String[] { "京宝行集团", "博瑞宝集团", "宝马销售集团" };
+	private static String[] regionNames = new String[] { "华北区", "华南区" };
+
+	private static String[] contractStatus = new String[] { "I", "C", "O" };
+
 	private static LocalDate now = LocalDate.now();
 	private static String dateString = DateUtil.dateToBasicISODate(now);
 	private static String[] salesPersonId = new String[] { "S023933", "S065754", "S066778" };
-
-	private ContractDataBuilder() {
-
-	}
 
 	public static List<Contract> buildcontractList() {
 		List<Contract> contractList = getInitContractList();
@@ -35,13 +41,21 @@ public class ContractDataBuilder {
 			c.setBasicInfo(bInfo);
 		}
 
+		//-------------------方弘文 DL-10006661------------------
+		for (String status : contractStatus) {
+			Contract c = createNewContract();
+			c.setProcessCode(status);
+			c.setContractStatus(status);			
+			contractList.add(0,c);
+		}
+		
 		return contractList;
 	}
 
 	private static List<BasicInfo> getBasicInfoList() {
 		List<BasicInfo> bInfoList = new ArrayList<BasicInfo>();
 
-		for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH * 3; i++) {
+		for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH * 12; i++) {
 			BasicInfo b = new BasicInfo();
 			// 订单号 orderNumber
 			b.setOrderNumber("O" + String.format("%012d", 3537 * (i + 1)));
@@ -69,7 +83,7 @@ public class ContractDataBuilder {
 		List<CustomerInfo> cInfoList = new ArrayList<CustomerInfo>();
 		Faker faker = new Faker(new Locale("zh-CN"));
 
-		for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH * 3; i++) {
+		for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH * 12; i++) {
 			CustomerInfo c = new CustomerInfo();
 			// 客户ID
 			c.setCustomerId("CID" + String.format("%06d", 537 * (i + 1)));
@@ -89,16 +103,90 @@ public class ContractDataBuilder {
 		// mock init contract list
 		List<Contract> cList = new ArrayList<>();
 
-		String[] contractStatus = new String[] { "I", "C", "O" };
+		for (int k = 0; k < dealerIds.length; k++) {
+			for (int j = 0; j < contractStatus.length; j++) {
+				for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH; i++) {
+					String contractId = "CT" + dateString + String.format("%06d", 1437 * (i + j + k + 1));
+					String processCode = contractStatus[j];
 
-		for (String cStatus : contractStatus) {
-			for (int i = 0; i < BMWPocConstants.CONTRACT_MOCK_LENGTH; i++) {
-				String contractNumber = "CT" + dateString + String.format("%06d", 437 * (i + 1));
-				String processStatus = cStatus;
-				cList.add(new Contract(contractNumber, processStatus, cStatus));
+					Contract c = new Contract();
+					c.setContractId(contractId);
+					c.setProcessCode(processCode);
+					c.setContractStatus(contractStatus[j]);
+
+					c.setDealerId(dealerIds[k]);
+					c.setDealerName(dealerNames[k]);
+
+					if (k == 0 || k == 1) {
+						c.setGroupId(groupIds[0]);
+						c.setGroupName(groupNames[0]);
+
+						c.setRegionId(regionIds[0]);
+						c.setRegionName(regionNames[0]);
+					}
+
+					if (k == 2) {
+						c.setGroupId(groupIds[1]);
+						c.setGroupName(groupNames[1]);
+
+						c.setRegionId(regionIds[1]);
+						c.setRegionName(regionNames[1]);
+					}
+
+					if (k == 3) {
+						c.setGroupId(groupIds[2]);
+						c.setGroupName(groupNames[2]);
+
+						c.setRegionId(regionIds[1]);
+						c.setRegionName(regionNames[1]);
+					}
+					cList.add(c);
+				}
 			}
 		}
 
 		return cList;
+	}
+	
+	private static Contract createNewContract() {
+		Contract cSpec = new Contract();
+		
+		cSpec.setDealerId(dealerIds[0]);
+		cSpec.setDealerName(dealerNames[0]);
+		cSpec.setGroupId(groupIds[0]);
+		cSpec.setGroupName(groupNames[0]);
+		cSpec.setRegionId(regionIds[0]);
+		cSpec.setRegionName(regionNames[0]);
+		cSpec.setContractId("CT20190804334112");
+		
+		CustomerInfo cusInfoSpec = new CustomerInfo();
+		// 客户ID
+		cusInfoSpec.setCustomerId("CID023933");
+		// 客户姓名
+		cusInfoSpec.setCustomerName("方弘文");
+		// 客户地址
+		cusInfoSpec.setCustomerAddress("北京市丰台区西罗园南里");
+		// 客户联系方式
+		cusInfoSpec.setCustomerContact("15827533242");
+		cSpec.setCustomerInfo(cusInfoSpec);
+		
+		BasicInfo basInfoSpec = new BasicInfo();
+		// 订单号 orderNumber
+		basInfoSpec.setOrderNumber("O000000000111");
+		// 订单日期 orderDate
+		basInfoSpec.setOrderDate(DateUtil.dateToString(now.minusDays(8)));
+		// 发票账户 invoiceNumber
+		basInfoSpec.setInvoiceNumber("I0045678336");
+		// 车架号 chassisNumber DUX20190802TEST01
+		basInfoSpec.setChassisNumber("DUX20190204TEST21");
+		// 销售员ID salesPersonId
+		basInfoSpec.setSalesPersonId(salesPersonId[0]);
+		// 预计交付日期 deliveryDate
+		basInfoSpec.setDeliveryDate(DateUtil.dateToString(now.minusDays(3)));
+		// 发票总金额 InvoiceTotal
+		basInfoSpec.setInvoiceTotal(450000d);
+		cSpec.setBasicInfo(basInfoSpec);
+		
+		return cSpec;
 	}
 }
--- src/main/java/com/bmw/service/ContractService.java
+++ src/main/java/com/bmw/service/ContractService.java
@@ -1,12 +1,15 @@
 package com.bmw.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bmw.common.BMWPocConstants;
 import com.bmw.model.Contract;
+import com.bmw.model.ContractTemplate;
 
 @Service
 public class ContractService {
@@ -15,14 +18,32 @@ public class ContractService {
 
 	public List<Contract> getPreContractList(String dealerId, String regionId, String groupId, String contractStatus) {
 
-		return contractList;
-	}
+		List<Contract> source = contractList;
 
+		if (StringUtils.isNotBlank(regionId)) {
+			source = source.stream().filter((Contract contract) -> contract.getRegionId().equals(regionId))
+					.collect(Collectors.toList());
+		}
+		if (StringUtils.isNotBlank(dealerId)) {
+			source = source.stream().filter((Contract contract) -> contract.getDealerId().equals(dealerId))
+					.collect(Collectors.toList());
+		}
+		if (StringUtils.isNotBlank(groupId)) {
+			source = source.stream().filter((Contract contract) -> contract.getGroupId().equals(groupId))
+					.collect(Collectors.toList());
+		}
+		if (StringUtils.isNotBlank(contractStatus)) {
+			source = source.stream().filter((Contract contract) -> contract.getContractStatus().equals(contractStatus))
+					.collect(Collectors.toList());
+		}
+
+		return source;
+	}
 
 	public int createContract(String contractId, Contract contractForm) {
 		int result = BMWPocConstants.REST_SUCCESS_CODE;
-		for(Contract contract : contractList) {
-			if(contractId != null && contractId.equals(contract.getContractId())) {
+		for (Contract contract : contractList) {
+			if (contractId != null && contractId.equals(contract.getContractId())) {
 				contract.setContractStatus(BMWPocConstants.CONTRACT_STATUS_CREATED);
 				contract.setProcessCode(BMWPocConstants.CONTRACT_STATUS_CREATED);
 			}
@@ -40,6 +61,11 @@ public class ContractService {
 			}
 		}
 		return result;
+	}
+
+	public ContractTemplate fillDataWithTemplate(String contractId, String templateId) {
+		ContractTemplate template = null;
+		return template;
 	}
 
 }
